c2224ea5087a8e51a680445cf46ceef2b7925bf6
RenataFebrianaJacom <renatafebrianajacom@gmal.com>
2021-04-04 23:17:43 +0800

    Buat file Input Output

--- InputOutput.java
+++ InputOutput.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class InputOutput{
  public static void main(String [] args){
+  Scanner scanner = new Scanner(System.in);
+  String nama;
+  System.out.print("Masukkan nama Anda: ");
+  nama = scanner.nextLine();
+  System.out.println("Hai, "+nama);
 
  }
 }
